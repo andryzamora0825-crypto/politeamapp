adeb1e5af050a5a6cd96a4f5bf82469e7f6e7241
Andry Zamora <andryzamora0825@gmail.com>
2026-05-14 21:16:54 -0500

    1.0 demo

--- src/components/VerifiedBadge.tsx
+++ src/components/VerifiedBadge.tsx
@@ -13,7 +13,7 @@ export function VerifiedBadge({ size = 16 }: VerifiedBadgeProps) {
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      title="Cuenta verificada"
+      aria-label="Cuenta verificada"
     >
       <circle cx="12" cy="12" r="12" fill="#0ea5e9" />
       <path
